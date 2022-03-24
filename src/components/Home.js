73c46bb38ef3c9439e9b95557380482cb48bc73d
Will <willchesson@gmail.com>
2022-03-24 10:12:58 -0400

    commit tree c6e67e0fcfda70d1c23fab10d343f2e4d3bcf6ae

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,16 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const hStyle = {
+    color: 'firebrick'
+  }
+
+  return (
+    <div id="home">
+      <h1 style={hStyle}>{name} is a Web Developer from {city}</h1>
+    
+    </div>
+  )
 }
 
 export default Home;
